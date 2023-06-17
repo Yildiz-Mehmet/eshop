16c19a00f02fd9d48d12a8a1451f50f3d8225a52
yıldız <mesel4461@gmail.com>
2023-06-18 01:37:39 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
     <div className="App">
-      <div className="header__logo"></div>
-      <div className="header__search"></div>
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">eSHOP</h2>
+      </div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
       <div className="header__nav">
         <div className="nav__item">
           <span className="nav__itemLineOne">Hello Guest</span>
@@ -15,7 +24,7 @@ function App() {
           <span className="nav__itemLineTwo">Shop</span>
         </div>
         <div className="nav__item">
-          <span className="nav__itemLineOne">Basket</span>
+          <ShoppingBasketIcon fontSize="large" />
           <span className="nav__itemLineTwo">0</span>
         </div>
       </div>
