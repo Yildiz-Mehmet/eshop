aa52f44f7569a71c33031ed2e19974b06ee77d06
yıldız <mesel4461@gmail.com>
2023-06-19 02:26:01 +0300

    as

--- src/Product.js
+++ src/Product.js
@@ -3,25 +3,23 @@ import "./Product.css";
 
 const Product = () => {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
-            facilis!
-          </p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>30</strong>
-          </p>
-          <div className="product__rating"> ⭐ ⭐</div>
-        </div>
-        <img
-          src="https://www.freeiconspng.com/thumbs/bag-png/clothing-bag-png-1.png"
-          alt="product-image"
-        />
-        <button>Add to Basket</button>
+    <div className="product">
+      <div className="product__info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+          facilis!
+        </p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>30</strong>
+        </p>
+        <div className="product__rating"> ⭐ ⭐</div>
       </div>
+      <img
+        src="https://www.freeiconspng.com/thumbs/bag-png/clothing-bag-png-1.png"
+        alt="product-image"
+      />
+      <button>Add to Basket</button>
     </div>
   );
 };
